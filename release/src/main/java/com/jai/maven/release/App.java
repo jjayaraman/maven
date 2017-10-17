daa274fd7d2ed2dbfa9ef5e1c7066d2be472e84a
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2017-10-17 17:57:37 +0100

    Code added

---
+++ release/src/main/java/com/jai/maven/release/App.java
@@ -0,0 +1,13 @@
+package com.jai.maven.release;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
